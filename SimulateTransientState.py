08b4c665666af219630ae7ec2e63d2e22836da2f
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2021-03-26 18:12:29 -0400

    transient state

--- SimulateTransientState.py
+++ SimulateTransientState.py
@@ -9,14 +9,9 @@ N_COHORTS = 500         # number of cohorts
 COHORT_POP_SIZE = 100   # size of each cohort
 
 # create multiple cohorts
-multiCohort = Cls.MultiCohort(
-    ids=range(N_COHORTS),   # [0, 1, 2 ..., N_COHORTS-1]
-    pop_sizes=[COHORT_POP_SIZE] * N_COHORTS,  # [COHORT_POP_SIZE, COHORT_POP_SIZE, ..., COHORT_POP_SIZE]
-    mortality_probs=[MORTALITY_PROB]*N_COHORTS  # [p, p, ....]
-)
+
 
 # simulate all cohorts
-multiCohort.simulate(n_time_steps=TIME_STEPS)
 
 # plot the sample paths
 Path.plot_sample_paths(
@@ -34,9 +29,5 @@ Hist.plot_histogram(
     y_label='Count')
 
 # print projected mean survival time (years)
-print('Projected mean survival time (years)',
-      multiCohort.multiCohortOutcomes.statMeanSurvivalTime.get_mean())
 
 # print projection interval
-print('95% projection (prediction, percentile, or uncertainty) interval of average survival time (years)',
-      multiCohort.multiCohortOutcomes.statMeanSurvivalTime.get_PI(alpha=0.05))
