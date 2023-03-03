ccc2465cc047e6919248b431cc2d1c71f45846d7
Reza Yaesoubi <reza.yaesoubi@yale.edu>
2023-03-03 12:00:33 -0500

    2023

--- SimulateSteadyState.py
+++ SimulateSteadyState.py
@@ -1,27 +1,27 @@
-import SurvivalModelClasses as Cls
-import SimPy.Plots.SamplePaths as Path
-import SimPy.Plots.Histogram as Hist
-import SimPy.Statistics as Stat
+import deampy.plots.histogram as hist
+import deampy.plots.sample_paths as path
+
+from SurvivalModelClasses import Cohort
 
 MORTALITY_PROB = 0.1    # annual probability of death
 TIME_STEPS = 100        # years
 SIM_POP_SIZE = 1000     # population size of the simulated cohort
 
 # create a cohort
-myCohort = Cls.Cohort(id=1, pop_size=SIM_POP_SIZE, mortality_prob=MORTALITY_PROB)
+myCohort = Cohort(id=1, pop_size=SIM_POP_SIZE, mortality_prob=MORTALITY_PROB)
 
 # simulate the cohort over the specified time steps
 myCohort.simulate(n_time_steps=TIME_STEPS)
 
 # plot the sample path
-Path.plot_sample_path(
+path.plot_sample_path(
     sample_path=myCohort.cohortOutcomes.nLivingPatients,
     title='Survival Curve',
     x_label='Time-Step (Year)',
     y_label='Number Survived')
 
 # plot the histogram
-Hist.plot_histogram(
+hist.plot_histogram(
     data=myCohort.cohortOutcomes.survivalTimes,
     title='Histogram of Patient Survival Time',
     x_label='Survival Time (Year)',
